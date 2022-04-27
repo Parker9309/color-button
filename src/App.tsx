18c06b73ddf7fa7dd007f20b3d6c94d5a2aa1d52
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-04-26 21:01:56 -0300

    chore(app): componentization to the component app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import {useState} from 'react';
-
+import {Button} from './Button';
 export function App() {
-  const [color, setColor] = useState('red');
-  const newButtonColor = color === 'red' ? 'blue' : 'red';
-
   return (
-    <div className="App">
-      <button 
-      style={{backgroundColor: color}} 
-      onClick={() => setColor(newButtonColor)} 
-      > 
-        Change to {newButtonColor}
-      </button>
-    </div>
+    <>
+      <Button />
+    </>
   );
 }
